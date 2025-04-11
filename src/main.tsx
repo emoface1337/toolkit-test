2db1dbf13232657d84b64205bdf3fd58ddb04588
emoface1337 <chvrches@mail.ru>
2025-04-11 14:54:06 +0300

    вернул strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { AppProvider } from './app';
 import './index.scss';
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
   <AppProvider />
-  // </StrictMode>
+  </StrictMode>
 );
